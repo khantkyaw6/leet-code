fc6c93b4295e949474f01b4ca4f8afc6413c445a
Khant Kyaw <khantkankei@gmail.com>
2024-06-25 22:43:10 +0630

    leet code for fun begin

---
+++ js basic/counter.js
@@ -0,0 +1,23 @@
+var createCounter = function (n) {
+	let count = n;
+
+	return function () {
+		return count++;
+	};
+};
+
+const counter = createCounter(10);
+counter(); // 10
+counter(); // 11
+counter(); // 12
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
